3ffd7a65d4666c128ee749d09e4612ca73d20571
Kazui Takahashi <tkhsh.schoen@gmail.com>
2018-06-14 15:10:25 +0900

    修正

--- deep_q_learning/deep_q_learning.py
+++ deep_q_learning/deep_q_learning.py
@@ -4,6 +4,8 @@
 import numpy as np
 import gym
 import tensorflow as tf
+from collections import deque
+import os
 
 episodes = 1000
 steps = 200
@@ -17,26 +19,30 @@ env = gym.make("CartPole-v0")
 
 class DQN:
   def __init__(self, state_dim, action_dim):
-    self.digitize_state_num = 6 # stateの離散化
     self.action_dim = action_dim
-    self.q_table = np.random.uniform(low = -1, high = 1, size=(state_dim ** self.digitize_state_num, action_dim))
-    self.learning_rate = 0.5
+    self.state_dim = state_dim
+    self.learning_rate = 0.01
+    self.minibatch_size = 32
+    self.replay_memory_size = 1000
+    self.gamma = 0.99 # 割引率
+    self.memory = deque(maxlen = self.replay_memory_size)
+    self.model_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "models")
+    self.model_name = "model.ckpt"
+    self.init_model()
 
+  def init_model(self):
     # 入力層
-    self.x = tf.placeholder(tf.float32, [None, state_dim])
-    # 隠れ層1
-    w_fc1 = tf.Variable(tf.truncated_normal([state_dim, 64], stddev=0.01))
-    b_fc1 = tf.Variable(tf.zeros([64]))
+    self.x = tf.placeholder(tf.float32, [None, self.state_dim])
+    # 隠れ層
+    w_fc1 = tf.Variable(tf.truncated_normal([self.state_dim, 32], stddev=0.01))
+    b_fc1 = tf.Variable(tf.zeros([32]))
     h_fc1 = tf.nn.relu(tf.matmul(self.x, w_fc1) + b_fc1)
-    # 隠れ層2
-    w_fc2 = tf.Variable(tf.truncated_normal([64, 32], stddev=0.01))
-    b_fc2 = tf.Variable(tf.zeros([32]))
-    h_fc2 = tf.nn.relu(tf.matmul(h_fc1, w_fc2) + b_fc2)
     # 出力層
-    w_out = tf.Variable(tf.truncated_normal([64, self.action_dim], stddev=0.01))
+    w_out = tf.Variable(tf.truncated_normal([32, self.action_dim], stddev=0.01))
     b_out = tf.Variable(tf.zeros([self.action_dim]))
-    self.y = tf.matmul(h_fc2, w_out) + b_out
+    self.y = tf.nn.softmax(tf.matmul(h_fc1, w_out) + b_out)
 
+    # 学習用
     self.y_ = tf.placeholder(tf.float32, [None, self.action_dim])
     self.loss = tf.reduce_mean(tf.square(self.y_ - self.y))
 
@@ -48,56 +54,80 @@ class DQN:
     self.sess = tf.Session()
     self.sess.run(tf.global_variables_initializer())
 
-  def digitize_state(self, observation):
-    #離散化
-    #[1:-1]は最初と最後の要素以外のスライス
-    cart_pos, cart_v, pole_angle, pole_v = observation
-    digitized_observation = [
-      np.digitize(cart_pos, bins = np.linspace(-2.4, 2.4, self.digitize_state_num + 1)[1:-1]),
-      np.digitize(cart_v, bins = np.linspace(-3.0, 3.0, self.digitize_state_num + 1)[1:-1]),
-      np.digitize(pole_angle, bins = np.linspace(-0.5, 0.5, self.digitize_state_num + 1)[1:-1]),
-      np.digitize(pole_v, bins = np.linspace(-2.0, 2.0, self.digitize_state_num + 1)[1:-1])]
-    return sum([x * (self.digitize_state_num ** i) for i, x in enumerate(digitized_observation)])
-
   def get_action(self, state_t, episode, learning = True):
     if(learning):
-      epsilon = 0.5 * (0.99 ** episode)
+      epsilon = 0.001 + 0.9 / (1.0 + episode)
       if epsilon <= np.random.uniform(0, 1):
-        action = np.argmax(self.q_table[state_t])
+        y = self.sess.run(self.y, feed_dict={self.x:state_t})
+        action = np.argmax(y)
       else:
         action = np.random.randint(self.action_dim)
     else:
-      action = np.argmax(self.q_table[state_t])
+      y = self.sess.run(self.y, feed_dict={self.x:state_t})
+      action = np.argmax(y)
 
     return action
 
-  def update(self, state_t, state_t_1, reward, terminal, action):
-    gamma = 0.99 # 割引率
-    alpha = 0.5 # 学習率
-    q_max = np.argmax(self.q_table[state_t_1])
-    q_max = self.q_table[state_t_1, q_max]
-    self.q_table[state_t, action] = (1 - alpha) * self.q_table[state_t, action] + alpha * (reward + gamma * q_max)
-
   def experience_replay(self):
+    state_minibatch = []
+    y_minibatch = []
 
-  def store_experience(self):
+    minibatch_size = min(len(self.memory), self.minibatch_size)
+    minibatch_indexes = np.random.randint(0, len(self.memory), minibatch_size)
+
+    for j in minibatch_indexes:
+      state_t, state_t_1, reward, terminal, action = self.memory[j]
+      #print self.memory[j]
+      # Q値
+      _y = self.sess.run(self.y, feed_dict={self.x:state_t})[0]
+      #print "---_y---"
+      #print _y
+      if terminal:
+        _y[action] = reward
+      else:
+        max_q =  np.max(self.sess.run(self.y, feed_dict={self.x:state_t_1}))
+        _y[action] = reward + self.gamma * max_q
+      state_minibatch.append(state_t[0])
+      y_minibatch.append(_y)
+
+    #print "---state_minibatch---"
+    #print state_minibatch
+    #print type(state_minibatch[0])
+    #print "---y_minibatch---"
+    #print y_minibatch
+    #print type(y_minibatch[0])
+    self.sess.run(self.training, feed_dict={self.x:state_minibatch, self.y_:y_minibatch})
+    self.current_loss = self.sess.run(self.loss, feed_dict={self.x:state_minibatch, self.y_:y_minibatch})
+    #print self.current_loss
+
+  def store_experience(self, state_t, state_t_1, reward, terminal, action):
+    self.memory.append((state_t, state_t_1, reward, terminal, action))
+
+print "--- learning start ---"
 
 agent = DQN(4, 2)
 
 for episode in range(episodes):
   observation = env.reset()
-  state_t_1 = agent.digitize_state(observation)
+  state_t_1 = observation.reshape((1, observation.size))
   state_t = state_t_1
   action = agent.get_action(state_t, episode)
   for step in range(steps):
     #env.render()
     observation, reward, terminal, info = env.step(action)
     state_t = state_t_1
-    state_t_1 = agent.digitize_state(observation)
+    state_t_1 = observation.reshape((1, observation.size))
     action = agent.get_action(state_t, episode)
     if terminal:
-      reward = -200
-    agent.update(state_t, state_t_1, reward, terminal, action)
+      if target_step < step:
+        reward = 1
+      else:
+        reward = -1
+    else:
+      reward = 0
+    agent.store_experience(state_t, state_t_1, reward, terminal, action)
+    if agent.minibatch_size < len(agent.memory):
+      agent.experience_replay()
     if terminal:
       print "episode:", episode
       print "finished at", step, "step"
@@ -105,15 +135,17 @@ for episode in range(episodes):
 
 print "--- learning result ---"
 observation = env.reset()
-state_t_1 = agent.digitize_state(observation)
+state_t_1 = observation.reshape((1, observation.size))
 state_t = state_t_1
 action = agent.get_action(state_t, episode, False)
 for step in range(steps):
   env.render()
   observation, reward, terminal, info = env.step(action)
   state_t = state_t_1
-  state_t_1 = agent.digitize_state(observation)
+  state_t_1 = observation.reshape((1, observation.size))
   action = agent.get_action(state_t, episode, False)
+  agent.store_experience(state_t, state_t_1, reward, terminal, action)
+  agent.experience_replay()
   if terminal:
     print "finished at", step, "step"
     break
