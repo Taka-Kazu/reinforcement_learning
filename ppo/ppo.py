e7487fe1ee1eb1ae9c0b04dc865adfcc96e11a1e
kazuki takahashi <tkhsh.schoen@gmail.com>
2018-10-07 23:33:42 +0900

    update

--- ppo/ppo.py
+++ ppo/ppo.py
@@ -21,10 +21,10 @@ LEARNING_RATE = 1e-4
 DECAY_STEPS = 1000
 DECAY_RATE = 0.98
 EPSILON = 0.2
-MIN_BATCH = 2048
-BUFFER_SIZE = MIN_BATCH * 10
-MAX_STEPS = 1000
-EPOCH = 5# 学習回数
+MIN_BATCH = 512
+BUFFER_SIZE = MIN_BATCH * 5
+MAX_STEPS = 200
+EPOCH = 3# 学習回数
 LOSS_V = 0.2
 LOSS_ENTROPY = 1e-3# beta
 LAMBDA = 0.95
@@ -42,13 +42,11 @@ MODEL_DIR = './models'
 SUMMARY_DIR = './results'
 
 #TIME_HORIZON = 32
-N_WORKERS = 1   # スレッドの数
-#Tmax = 3*N_WORKERS   # 各スレッドの更新ステップ間隔
 
 # ε-greedyのパラメータ
 EPS_START = 1
 EPS_END = 0.01
-EPS_STEPS = 500*N_WORKERS*MAX_STEPS
+EPS_STEPS = 500*MAX_STEPS
 
 TARGET_SCORE = 1000#-250
 GLOBAL_EP = 0
@@ -81,21 +79,21 @@ class PPONet(object):
     self.global_step = tf.Variable(0, trainable=False)# 記録用
     self.learning_rate = tf.train.exponential_decay(LEARNING_RATE, self.global_step, DECAY_STEPS, DECAY_RATE, staircase=True)
 
-    self.s_t = tf.placeholder(tf.float32, shape=(None, NUM_STATES))
-    self.a_t = tf.placeholder(tf.float32, shape=(None, NUM_ACTIONS))
-    self.r_t = tf.placeholder(tf.float32, shape=(None, 1))
+    self.s_t = tf.placeholder(tf.float32, shape=(None, NUM_STATES), name="s_t")
+    self.a_t = tf.placeholder(tf.float32, shape=(None, NUM_ACTIONS), name="a_t")
+    self.r_t = tf.placeholder(tf.float32, shape=(None, 1), name="r_t")
 
     # alpha, betaは分布を表すパラメータ
-    self.alpha, self.beta, self.v, self.params = self._build_net('pi', trainable=True)
-    self.old_alpha, self.old_beta, self.old_v, self.old_params = self._build_net('old_pi', trainable=False)
-    self.train_queue = [[], [], [], [], []]# s, a, r, s', s' terminal mask
-    self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-    # paramのコピー操作
-    self.assign_op = [self.old_params[i].assign(self.params[i]) for i in range(len(self.params))]
-    self.graph = self._build_graph()
+    with tf.name_scope("pi"):
+      #self.alpha, self.beta, self.v, self.params = self._build_net('pi', trainable=True)
+      self.alpha, self.beta, self.params = self._build_net('pi', trainable=True)
 
-  def _build_net(self, name, trainable):
-    with tf.variable_scope(name):
+    with tf.name_scope("old_pi"):
+      #self.old_alpha, self.old_beta, self.old_v, self.old_params = self._build_net('old_pi', trainable=False)
+      self.old_alpha, self.old_beta, self.old_params = self._build_net('old_pi', trainable=False)
+
+    with tf.name_scope("critic"):
+      weight_init = tf.random_normal_initializer(0., .1)
       step_size = tf.shape(self.s_t)[:1]
       s = tf.expand_dims(self.s_t, axis=1, name="timely_input")
       rnn_cell = tf.nn.rnn_cell.LSTMCell(num_units=CELL_SIZE, name="LSTM")
@@ -108,40 +106,54 @@ class PPONet(object):
                                      )
 
       cell_out = tf.reshape(outputs, [-1, CELL_SIZE], name="flatten_outputs")
+      l4 = tf.layers.dense(cell_out, NUM_HIDDENS[2], tf.nn.relu, trainable=True, name="l4")
+      self.v = tf.layers.dense(l4, 1, trainable=True, name="value")
 
-      l1 = tf.layers.dense(cell_out, NUM_HIDDENS[0], tf.nn.relu, trainable=trainable, name="l1")
+    self.train_queue = [[], [], [], [], []]# s, a, r, s', s' terminal mask
+    self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+    # paramのコピー操作
+    self.assign_op = [self.old_params[i].assign(self.params[i]) for i in range(len(self.params))]
+    self.graph = self._build_graph()
+
+  def _build_net(self, name, trainable):
+    with tf.variable_scope(name):
+      l1 = tf.layers.dense(self.s_t, NUM_HIDDENS[0], tf.nn.relu, trainable=trainable, name="l1")
       #l2 = tf.layers.dense(l1, NUM_HIDDENS[1], tf.nn.relu, trainable=trainable, name="l2")
-      l3 = tf.layers.dense(l1, NUM_HIDDENS[2], tf.nn.relu, trainable=trainable, name="l3")
-      alpha = tf.layers.dense(l3, NUM_ACTIONS, tf.nn.softplus, trainable=trainable, name="actor_alpha")
-      beta = tf.layers.dense(l3, NUM_ACTIONS, tf.nn.softplus, trainable=trainable, name="actor_beta")
-      value = tf.layers.dense(l3, 1, trainable=trainable, name="critic")
+      #l3 = tf.layers.dense(l2, NUM_HIDDENS[2], tf.nn.relu, trainable=trainable, name="l3")
+      alpha = tf.layers.dense(l1, NUM_ACTIONS, tf.nn.softplus, trainable=trainable, name="actor_alpha")
+      beta = tf.layers.dense(l1, NUM_ACTIONS, tf.nn.softplus, trainable=trainable, name="actor_beta")
     params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
-    return alpha, beta, value, params
+    #return alpha, beta, value, params
+    return alpha, beta, params
 
   def _build_graph(self):
-    x = (self.a_t - A_BOUNDS[0]) / (-A_BOUNDS[0] + A_BOUNDS[1])
+    with tf.name_scope("action"):
+      x = (self.a_t - A_BOUNDS[0]) / (-A_BOUNDS[0] + A_BOUNDS[1])
 
-    beta_dist = tf.contrib.distributions.Beta(self.alpha + 1, self.beta + 1)
-    self.prob = beta_dist.prob(x) + 1e-8
+      beta_dist = tf.distributions.Beta(self.alpha + 1, self.beta + 1)
+      self.prob = beta_dist.prob(x) + 1e-8
 
-    beta_dist_old = tf.contrib.distributions.Beta(self.old_alpha + 1, self.old_beta + 1)
-    self.prob_old = tf.stop_gradient(beta_dist_old.prob(x) + 1e-8)
-    self.A = beta_dist.sample(1) * (-A_BOUNDS[0] + A_BOUNDS[1]) + A_BOUNDS[0]
-    # 誤差関数
-    self.advantage = self.r_t - self.v
-    r_theta = self.prob / self.prob_old
-    # CPI:Conservative Policy Iteration
-    loss_CPI = r_theta * tf.stop_gradient(self.advantage)
-    r_clip = tf.clip_by_value(r_theta, 1 - EPSILON, 1 + EPSILON)
-    clipped_loss_CPI = r_clip * tf.stop_gradient(self.advantage)
-    self.loss_CLIP = -tf.reduce_mean(tf.minimum(loss_CPI, clipped_loss_CPI))
+      beta_dist_old = tf.contrib.distributions.Beta(self.old_alpha + 1, self.old_beta + 1)
+      self.prob_old = tf.stop_gradient(beta_dist_old.prob(x) + 1e-8)
 
-    self.loss_value = LOSS_V * tf.reduce_mean(tf.square(self.advantage))
-    self.entropy = LOSS_ENTROPY * tf.reduce_mean(beta_dist.entropy())
-    self.loss_total = self.loss_CLIP + self.loss_value - self.entropy
+      self.A = beta_dist.sample(1) * (-A_BOUNDS[0] + A_BOUNDS[1]) + A_BOUNDS[0]
 
-    minimize = self.optimizer.minimize(self.loss_total, global_step=self.global_step)
-    return minimize
+    with tf.name_scope("actor_loss"):
+      # 誤差関数
+      self.advantage = self.r_t - self.v
+      r_theta = self.prob / self.prob_old
+      # CPI:Conservative Policy Iteration
+      loss_CPI = r_theta * tf.stop_gradient(self.advantage)
+      r_clip = tf.clip_by_value(r_theta, 1 - EPSILON, 1 + EPSILON)
+      clipped_loss_CPI = r_clip * tf.stop_gradient(self.advantage)
+      self.loss_CLIP = -tf.reduce_mean(tf.minimum(loss_CPI, clipped_loss_CPI))
+
+      self.loss_value = LOSS_V * tf.reduce_mean(tf.square(self.advantage))
+      self.entropy = LOSS_ENTROPY * tf.reduce_mean(beta_dist.entropy())
+      self.loss_total = self.loss_CLIP + self.loss_value - self.entropy
+
+      minimize = self.optimizer.minimize(self.loss_total, global_step=self.global_step)
+      return minimize
 
   def update_parameter_server(self):
     if len(self.train_queue[0]) < BUFFER_SIZE:
@@ -153,7 +165,8 @@ class PPONet(object):
     for i in range(EPOCH):
       print("EPOCH:" + str(i+1))
       n_batches = int(BUFFER_SIZE / MIN_BATCH)
-      batch = np.random.permutation(Buffer)
+      #batch = np.random.permutation(Buffer)
+      batch = Buffer
       for n in range(n_batches):
         s, a, r, s_, s_mask = np.array(batch[n * MIN_BATCH: (n + 1) * MIN_BATCH]).T
         s = np.vstack(s)
@@ -162,7 +175,7 @@ class PPONet(object):
         s_ = np.vstack(s_)
         s_mask = np.vstack(s_mask)
 
-        v = self.sess.run(self.v, feed_dict={self.s_t:s_})
+        v = self.get_v(s)
 
         r = r + LAMBDA * GAMMA_N * v * s_mask
 
@@ -198,6 +211,13 @@ class PPONet(object):
       self.train_queue[3].append(s_)
       self.train_queue[4].append(1.)
 
+  def get_v(self, s):
+    v = self.sess.run(self.v, feed_dict={self.s_t: s})
+    return v
+
+  def reset_lstm(self):
+    self.sess.run(self.init_state)
+
 class Agent:
   def __init__(self, brain):
     self.brain = brain
@@ -225,7 +245,6 @@ class Agent:
       return s, a, self.R, s_
 
     self.memory.append((s, a, r, s_))
-
     # 前ステップの「時間割引Nステップ分の総報酬R」を使用して、現ステップのRを計算
     self.R = (self.R + LAMBDA * r * GAMMA_N) / GAMMA     # r0はあとで引き算している、この式はヤロミルさんのサイトを参照
 
@@ -249,7 +268,6 @@ class Agent:
       self.memory.pop(0)
       #print([self.memory[i][2] for i in range(len(self.memory))])
 
-
 class Environment:
   total_reward_vec = np.array([0 for i in range(20)])  # 総報酬を20試行分格納して、平均総報酬をもとめる
 
@@ -268,9 +286,10 @@ class Environment:
     s = self.env.reset().reshape(-1)
     R = 0
     step = 0
+    self.agent.brain.reset_lstm()
     while True:
-      #if self.name == 'W_0':
-      #    self.env.render()
+      if self.name == 'W_0':
+          self.env.render()
       a = self.agent.act(s)
       s_, r, done, info = self.env.step(a)
       s_ = s_.reshape(-1)
@@ -284,7 +303,7 @@ class Environment:
           s_ = None
 
       # 報酬と経験を、Brainにプッシュ
-      r=(r+8)/8.0
+      r=(r+8)/8
       self.agent.advantage_push_brain(s, a, r, s_)
       s = s_
       R += r
@@ -318,35 +337,20 @@ class Worker_thread:
       self.environment = Environment(thread_name, brain)
 
     def run(self):
-      while True:
+      while true:
         self.environment.run()
-        if isLearned:
+        if islearned:
             break
 
 # -- main -----------------------------
 if __name__ == "__main__":
   frames = GLOBAL_EP * MAX_STEPS
   isLearned = False
-  config = tf.ConfigProto(
-    gpu_options=tf.GPUOptions(
-      visible_device_list="0",
-      per_process_gpu_memory_fraction=0.5,
-      #allow_growth=True
-      )
-    #allow_soft_placement = True
-  )
-  #SESS = tf.Session()#config = config)
+
   with tf.Session() as SESS:
-  #with tf.device("/cpu:0"):
-  #with tf.device("/gpu:0"):
     brain = PPONet(SESS)
-    workers = []
-    for i in range(N_WORKERS):
-      worker_name = 'W_%i' % i
-      workers.append(Worker_thread(thread_name=worker_name, brain=brain))
 
     summary_ops, summary_vars = build_summaries()
-    COORD = tf.train.Coordinator()
     SESS.run(tf.global_variables_initializer())
     writer = tf.summary.FileWriter(SUMMARY_DIR,SESS.graph)
     saver = tf.train.Saver()
@@ -356,10 +360,8 @@ if __name__ == "__main__":
       saver.restore(SESS,nn_model)
       print("Model restored!!")
 
-    worker_threads = []
-    for worker in workers:
-      job = lambda: worker.run()
-      t = threading.Thread(target=job)
-      t.start()
-      worker_threads.append(t)
-    COORD.join(worker_threads)
+    environment = Environment("W_0", brain)
+    while True:
+      environment.run()
+      if isLearned:
+          break
